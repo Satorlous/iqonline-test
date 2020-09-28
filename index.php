eaaaad834bc15b8c0fd0d73951b7254b41aabfde
Satorlous <n.leppik@gmail.com>
2020-09-28 14:08:32 +0500

    Initial

---
+++ index.php
@@ -0,0 +1,126 @@
+<!doctype html>
+<html lang="ru">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="src/styles/bootstrap.css">
+    <link rel="stylesheet" href="src/styles/main.css">
+    <title>WBP</title>
+</head>
+
+<body>
+
+<!-- BODY START -->
+<div class="wrapper">
+    <nav>
+        <div class="container-fluid p-0">
+            <div class="navbar-logo">
+                <div class="row">
+                    <div class="col-logo">
+                        <img src="src/media/World_Bank_logo.svg" class="logo" alt="">
+                    </div>
+                    <div class="col-4 post-logo">
+                        <div class="row">
+                            <div class="col-12 pl__f-line">WORLD BANK</div>
+                            <div class="col-12 pl__s-line">Publications</div>
+                        </div>
+                    </div>
+                    <div class="col text-right my-auto phone">
+                        <div class="col-auto phone__f-line">8-800-100-5005</div>
+                        <div class="col-auto phone__s-line">+7 (3452) 522-000</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="navbar-dark nav-bg-dark">
+            <ul class="nav nav-fill">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Кредитные карты</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Вклады</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Дебетовая карта</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Страхование</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Друзья</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Интернет-банк</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <main class="conatiner main-content">
+        <div class="row">
+            <div class="col-auto" aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="#">Главная</a></li>
+                    <li class="breadcrumb-item"><a href="#">Вклады</a></li>
+                    <li class="breadcrumb-item active">Калькулятор</li>
+                </ol>
+            </div>
+        </div>
+
+
+        <div class="row">
+            <div class="col-lg-8">
+                <div class="calculator">
+                    <div class="caption">Калькулятор</div>
+                    <form action="">
+                        <div class="row"></div>
+                        <div class="row"></div>
+                        <div class="row"></div>
+                        <div class="row"></div>
+                        <div class="row"></div>
+                        <div class="row"></div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </main>
+</div>
+
+<footer class="footer">
+    <div class="container">
+        <div class="row footer__links">
+            <div class="col-auto">
+                <a href="#">Кредитные карты</a>
+            </div>
+            <div class="col-auto">
+                <a href="#">Вклады</a>
+            </div>
+            <div class="col-auto">
+                <a href="#">Дебетовая карта</a>
+            </div>
+            <div class="col-auto">
+                <a href="#">Страхование</a>
+            </div>
+            <div class="col-auto">
+                <a href="#">Друзья</a>
+            </div>
+            <div class="col-auto">
+                <a href="#">Интернет-банк</a>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<!-- BODY END -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+        integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI"
+        crossorigin="anonymous"></script>
+</body>
+</html>
